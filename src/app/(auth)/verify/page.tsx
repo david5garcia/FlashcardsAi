c3b0bb56cb1629a58bd9a40d4b1631f4cbc1f214
david5garcia <davidgballester@gmail.com>
2025-01-02 15:38:09 +0100

    update style verification page

--- src/app/(auth)/verify/page.tsx
+++ src/app/(auth)/verify/page.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/shared/button";
+import Card from "@/components/shared/card";
 import prisma from "@/lib/db/db";
 import Link from "next/link";
 
@@ -32,13 +34,14 @@ const Verify = async ({
   });
 
   return (
-    <div>
-      <h1>Account Verified</h1>
-      <p>
-        Your account {user.email} has been verified. You can now
-        <Link href="/login">login</Link>
-      </p>
-    </div>
+    <Card className="bg-white px-4 py-10 flex flex-col gap-4 text-center items-center max-w-[700px] mt-4 mx-auto">
+      <h1 className="text-3xl">Account Verified</h1>
+      <p>Your account {user.email} has been verified.</p>
+
+      <Link href="/login">
+        <Button>Login</Button>
+      </Link>
+    </Card>
   );
 };
 
